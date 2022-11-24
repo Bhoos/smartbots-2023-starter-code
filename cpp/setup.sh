4a348a2446f7e3714d56de5ea3678c7ed68fc049
tilakchad111 <tilakchad111@gmail.com>
2022-11-24 23:15:06 +0545

    starter code for c++

---
+++ cpp/setup.sh
@@ -0,0 +1,4 @@
+mkdir include 
+cd ./include/
+curl -LGO https://raw.githubusercontent.com/yhirose/cpp-httplib/master/httplib.h
+curl -LGO https://raw.githubusercontent.com/nlohmann/json/develop/single_include/nlohmann/json.hpp
